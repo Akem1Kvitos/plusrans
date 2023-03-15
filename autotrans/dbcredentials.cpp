ca964a1ff8beeb82ba1cfea836aaefe47e742977
Danila Kislitsyn <dangost16@gmail.com>
2023-03-16 01:10:39 +0300

    remove c++ proj

--- autotrans/dbcredentials.cpp
+++
@@ -1,9 +0,0 @@
-#include "dbcredentials.h"
-
-DBCredentials::DBCredentials(QString ip, QString user, QString pass, QString dbname)
-{
-    this->host = ip + ":3306";
-    this->user = user;
-    this->password = pass;
-    this->dbname = dbname;
-}
